cf6401176d1df2c5abeac787696ca55224e77471
Bhavya19g <115550205+Bhavya19g@users.noreply.github.com>
2023-02-06 15:27:58 +0530

    script.sh

---
+++ script.sh
@@ -0,0 +1,9 @@
+git clone https://github.com/Bhavya19g/Agriculture.git
+cd Agriculture
+pip3 install -r requirements.txt
+python3 app.py
+
+git clone https://github.com/Bhavya19g/Fuel.git
+cd Fuel
+pip3 install -r requirements.txt
+python3 app.py
