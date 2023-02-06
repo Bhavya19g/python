4bf9ef3b3a0c6935330a4830eb9d3a149abfe55f
Bhavya19g <115550205+Bhavya19g@users.noreply.github.com>
2023-02-06 15:40:52 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,9 +1,9 @@
 git clone https://github.com/Bhavya19g/Agriculture.git
 cd Agriculture
 pip3 install -r requirements.txt
-python3 app.py
+screen -m -d python3 app.py
 
 git clone https://github.com/Bhavya19g/Fuel.git
 cd Fuel
 pip3 install -r requirements.txt
-python3 app.py
+screen -m -d python3 app.py
